b5317108b61f6979eadd2dbee78b840359f1b2a3
genleay <43116069+genleay@users.noreply.github.com>
2018-09-22 13:20:03 +0800

    skill ui

---
+++ simulator/enemy.class.js
@@ -0,0 +1,28 @@
+class Enemy{
+    constructor(enemy_json){
+        this.name = enemy_json['enemy']['property']['name'];
+        this.kurasu = "";
+        
+        this.getName = function(lang = 'jp'){
+            return this.name[lang];
+        };
+        this.setName = function(name_array){
+            this.name = name_array;
+        };
+        
+        this.setKurasu = function(class_name){
+            this.kurasu = class_name;
+        };
+        
+        this.getSetting = function(){
+            return {
+                getBattleServantSprite: function(){
+                    return {
+                        getHeight: function(){ return enemy_json['enemy']['setting']['image']['battle']['servant-sprite']['height']; },
+                        getGroundPoint: function(){ return enemy_json['enemy']['setting']['image']['battle']['servant-sprite']['ground-point']; }
+                    };
+                },
+            };
+        };
+    }
+}
